d869a684c48a99f7981576ffcc202ab226322946
Billy Ernest <bernest@gridprotectionalliance.org>
2020-06-10 10:53:51 -0400

    Added databased logging.

---
+++ PQDigest/PQDigest.sql
@@ -0,0 +1,47 @@
+﻿--//******************************************************************************************************
+--//  PQDigest.sql - Gbtc
+--//
+--//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+--//
+--//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+--//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+--//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+--//  file except in compliance with the License. You may obtain a copy of the License at:
+--//
+--//      http://opensource.org/licenses/MIT
+--//
+--//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+--//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+--//  License for the specific language governing permissions and limitations.
+--//
+--//  Code Modification History:
+--//  ----------------------------------------------------------------------------------------------------
+--//  06/10/2020 - Billy Ernest
+--//       Generated original version of source code.
+--//
+--//******************************************************************************************************
+
+USE [master]
+GO
+CREATE DATABASE PQDigest
+GO
+
+USE PQDIGEST
+GO
+
+CREATE TABLE [Log] (
+
+   [Id] int IDENTITY(1,1) NOT NULL,
+   [Message] nvarchar(max) NULL,
+   [MessageTemplate] nvarchar(max) NULL,
+   [Level] nvarchar(128) NULL,
+   [TimeStamp] datetimeoffset(7) NOT NULL,
+   [Exception] nvarchar(max) NULL,
+   [Properties] xml NULL,
+   [LogEvent] nvarchar(max) NULL
+
+   CONSTRAINT [PK_Log]
+     PRIMARY KEY CLUSTERED ([Id] ASC)
+
+)
+GO
--- PQDigest/Startup.cs
+++ PQDigest/Startup.cs
@@ -1,3 +1,26 @@
+//******************************************************************************************************
+//  Startup.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  06/09/2020 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +46,7 @@ namespace PQDigest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            //services.AddAuthentication("NTLM");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,12 +59,21 @@ namespace PQDigest
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+
             }
 
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.Use((context, next) => {
+
+                return next.Invoke();
+            });
+
+            //app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

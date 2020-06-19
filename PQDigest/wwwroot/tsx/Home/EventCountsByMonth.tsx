d352966599ded59984e5999d0016e6eadf8ddf5e
Billy Ernest <bernest@gridprotectionalliance.org>
2020-06-19 08:55:59 -0400

    Completed event count chart

--- PQDigest/wwwroot/tsx/Home/EventCountsByMonth.tsx
+++ PQDigest/wwwroot/tsx/Home/EventCountsByMonth.tsx
@@ -33,15 +33,16 @@ interface iData {
     total: number
 }
 const EventCountsByMonth = (props: { Width: number, Height: number }) => {
-    let margin = { top: 20, right: 20, bottom: 20, left: 20 };
+    let margin = { top: 20, right: 30, bottom: 50, left: 30 };
     let svgWidth = props.Width - margin.left - margin.right;
-    let svgHeight = props.Height - margin.left - margin.right;
+    let svgHeight = props.Height - margin.top - margin.bottom;
 
     const [totalPath, setTotalPath] = React.useState<string>('');
     const [paths, setPaths] = React.useState<Array<JSX.Element>>([]);
     const [hidden, setHidden] = React.useState<boolean>(false);
     const [axisPaths, setAxisPaths] = React.useState<Array<JSX.Element>>([]);
     const [axisTicks, setAxisTicks] = React.useState<Array<JSX.Element>>([]);
+    const [yaxisTicks, setYAxisTicks] = React.useState<Array<JSX.Element>>([]);
 
     React.useEffect(() => {
         //setHidden(true);
@@ -71,6 +72,17 @@ const EventCountsByMonth = (props: { Width: number, Height: number }) => {
         //let x = scaleOrdinal().rangeRound([margin.left, svgWidth + margin.left]).domain(data.map(x => x.month));
         let y = scaleLinear().rangeRound([svgHeight, margin.top]).domain([0, max(data.map(x => x.total))]);
 
+        let yticks = [];
+        for (let i = 0; i < y.domain()[1]; i = i + 5) {
+            yticks.push(
+                <g key={i} className="tick" opacity="1" transform={`translate(${margin.left},${y(i)})`}>
+                    <line stroke="gray" x2="-6"></line>
+                    <text fill="gray" x="-30" dy="0.32em">{i}</text>
+                </g>);
+
+        }
+        setYAxisTicks(yticks);
+
         let ticks = data.map((x, index) => {
             return (
                 <g key={index} className="tick" opacity="1" transform={`translate(${margin.left + svgWidth * (index + 1) / 12},${svgHeight})`}>
@@ -95,7 +107,7 @@ const EventCountsByMonth = (props: { Width: number, Height: number }) => {
 
         let boxes = data.map((x, index) => {
             return (
-                <g key={index}>
+                <g key={index} onClick={() => { }}>
                     <rect x={margin.left + (svgWidth * ((data.map(datum => datum.month).indexOf(x.month) + 0.35)) / 12)} y={y(x.sag)} width={20} height={svgHeight - y(x.sag)} fill={getColor('sag')}/>
                     <rect x={margin.left + (svgWidth * ((data.map(datum => datum.month).indexOf(x.month) + 0.35)) / 12)} y={y(x.sag) - (svgHeight - y(x.swell))} width={20} height={svgHeight - y(x.swell)} fill={getColor('swell')}/>
                     <rect x={margin.left + (svgWidth * ((data.map(datum => datum.month).indexOf(x.month) + 0.35)) / 12)} y={y(x.sag) - (svgHeight - y(x.swell)) - (svgHeight - y(x.transient))} width={20} height={svgHeight - y(x.transient)} fill={getColor('Transient')}/>
@@ -122,25 +134,35 @@ const EventCountsByMonth = (props: { Width: number, Height: number }) => {
     return (
         <div style={{ height: props.Height, width: props.Width, textAlign: 'center' /*, margin: '0x', padding: '0px'*/ }} hidden={hidden}>
             <svg width={props.Width} height={props.Height}>
+                {/* Chart header */}
                 <text y={15} x={props.Width / 2 - 125} stroke="gray">Historical Event Counts by Month</text>
+
+                {/* Chart borders */}
                 <line x1={margin.left} x2={svgWidth + margin.right} y1={svgHeight} y2={svgHeight} stroke="gray" strokeWidth={2} />
                 <line x1={margin.left} x2={svgWidth + margin.right} y1={margin.top} y2={margin.top} stroke="gray" strokeWidth={2} />
                 <line x1={margin.left} x2={margin.left} y1={margin.top} y2={svgHeight} stroke="gray" strokeWidth={2} />
                 <line x1={svgWidth + margin.right} x2={svgWidth + margin.right} y1={margin.top} y2={svgHeight} stroke="gray" strokeWidth={2} />
 
-                <g>
-                    {paths}
-                </g>
-                <g>
-                    <path fill='none' strokeLinejoin='round' strokeWidth='3' stroke='darkblue' d={totalPath} />
-                </g>
+                {/* Chart Legend */}
+                <rect x={margin.left + 10} y={svgHeight + 25} width={15} height={15} fill={getColor('sag')} />
+                <text y={svgHeight + 38} x={margin.left + 30} stroke="gray">Sag</text>
+                <rect x={margin.left + 70} y={svgHeight + 25} width={15} height={15} fill={getColor('swell')} />
+                <text y={svgHeight + 38} x={margin.left + 90} stroke="gray">Swell</text>
+                <rect x={margin.left + 140} y={svgHeight + 25} width={15} height={15} fill={getColor('transient')} />
+                <text y={svgHeight + 38} x={margin.left + 160} stroke="gray">Transient</text>
+                <rect x={margin.left + 240} y={svgHeight + 25} width={15} height={15} fill={getColor('fault')} />
+                <text y={svgHeight + 38} x={margin.left + 260} stroke="gray">Fault</text>
+                <rect x={margin.left + 320} y={svgHeight + 25} width={15} height={15} fill={getColor('interruption')} />
+                <text y={svgHeight + 38} x={margin.left + 340} stroke="gray">Interruption</text>
 
-                <g>
-                    <g className="tick" opacity="1" transform={`translate(${margin.left},${svgHeight})`}>
-                        <line y2="6" stroke="gray" strokeWidth={2}></line>
-                    </g>
-                    {axisTicks}
+                {paths}
+                <path fill='none' strokeLinejoin='round' strokeWidth='3' stroke='darkblue' d={totalPath} />
+
+                <g className="tick" opacity="1" transform={`translate(${margin.left},${svgHeight})`}>
+                    <line y2="6" stroke="gray" strokeWidth={2}></line>
                 </g>
+                {axisTicks}
+                {yaxisTicks}
                 <g>{axisPaths}</g>
             </svg>
 

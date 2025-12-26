e3395c09cd09063031b355d9da34a1e0b66ab69e
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-12-26 13:28:32 -0500

    cleaned up helper methods

--- PQDigest/Security/HelperMethods.cs
+++ PQDigest/Security/HelperMethods.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
+﻿//******************************************************************************************************
+//  HelperMethods.tsx - Gbtc
+//
+//  Copyright © 2025, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  12/26/2020 - Gabriel Santos
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
 using System.Security.Claims;
-using System.Threading.Tasks;
-using Gemstone.Configuration;
 using Gemstone.Data;
 using Gemstone.Data.Model;
-using Gemstone.Reflection;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using openXDA.Model;
-using PQDigest.Models;
 using SystemCenter.Model;
 
 namespace PQDigest.Security
